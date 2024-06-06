7ebba2715707bb4d55b7b1aabe49472b9da82735
chandanbhagat7s <bhagatchandan287@gmail.com>
2024-06-06 19:23:42 +0530

    more work on frontedn

--- Frontend/src/Test.jsx
+++ Frontend/src/Test.jsx
@@ -2,9 +2,77 @@ import React, { useState } from "react";
 
 const Invoice = () => {
   const [count, setCount] = useState(0);
+  const [tableData, setTableData] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  let rest = [];
+  const handlechange = (e, id) => {
+    let obj = tableData.filter((el) => {
+      if (el.id !== id) {
+        rest.push(el);
+      }
+      return el.id == id;
+    });
+    const { name, value } = e.target;
+
+    let newObj = {
+      ...obj[0],
+      [name]: value,
+    };
+    console.log(newObj);
+
+    let calculated = {
+      netAmount: Number(newObj.unitPrice) * Number(newObj.quantity) || 0,
+      netTax: Number(newObj.unitTax) * Number(newObj.quantity) || 0,
+
+      ftaxcgstAmout:
+        Number(newObj.unitPrice) *
+          Number(newObj.quantity) *
+          (newObj.fcgst / 100) || 0,
+      ftaxsgstAmout:
+        Number(newObj.unitPrice) *
+          Number(newObj.quantity) *
+          (newObj.fsgst / 100) || 0,
+      staxcgstAmout:
+        Number(newObj.unitTax) *
+          Number(newObj.quantity) *
+          (newObj.scgst / 100) || 0,
+      staxsgstAmout:
+        Number(newObj.unitTax) *
+          Number(newObj.quantity) *
+          (newObj.ssgst / 100) || 0,
+      ftotalAmount:
+        Number(newObj.unitPrice) *
+          Number(newObj.quantity) *
+          (newObj.fsgst / 100) +
+        Number(newObj.unitPrice) *
+          Number(newObj.quantity) *
+          (newObj.fcgst / 100) +
+        Number(newObj.unitPrice) * Number(newObj.quantity),
+      stotalAmount:
+        Number(newObj.unitTax) *
+          Number(newObj.quantity) *
+          (newObj.scgst / 100) +
+        Number(newObj.unitTax) *
+          Number(newObj.quantity) *
+          (newObj.ssgst / 100) +
+        Number(newObj.unitTax) * Number(newObj.quantity),
+    };
+
+    newObj = { ...newObj, ...calculated };
+    console.log("calculated", calculated, newObj);
+    const sortedObject = [...rest, newObj].sort((a, b) => a.id - b.id);
+    let totalcount = 0;
+    [...rest, newObj].map((el) => {
+      totalcount = totalcount + el.ftotalAmount + el.stotalAmount;
+    });
+    setTotal(totalcount);
+    setTableData([...sortedObject]);
+    console.log(tableData);
+  };
 
   return (
-    <div className="max-w-4xl mx-auto p-4 border rounded shadow-lg">
+    <div className="max-w-5xl mx-auto p-4 border rounded shadow-lg">
       <div className="flex justify-between items-center">
         <div>
           <p>click to select image for logo &darr;</p>
@@ -158,95 +226,244 @@ const Invoice = () => {
         </div>
       </div>
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Sl. No</th>
-            <th>Description</th>
-            <th>Unit Price</th>
-            <th>Qty</th>
-            <th>Net Amount</th>
-            <th>Tax Rate</th>
-            <th>Tax Type</th>
-            <th>Tax Amount</th>
-            <th>Total Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!count && (
+      <div className="overflow-scroll">
+        <table
+          cellPadding={"1"}
+          cellSpacing={"4"}
+          className="table-auto border-collapse border border-gray-400"
+        >
+          <thead className="bg-gray-200">
             <tr>
-              <td colSpan="9">
-                <div className="flex justify-around my-20">
-                  <div>Please enter how many details you have</div>
-                  <input
-                    type="number"
-                    placeholder="how many orders you have"
-                    className="p-2 focus:outline-none focus:border-1 focus:border focus:border-gray-300"
-                    onChange={(e) => {
-                      console.log(e.target.value);
-                      setCount(Number(e.target.value));
-                    }}
-                  />
-                </div>
-              </td>
+              <th>Sl. No</th>
+              <th>Description</th>
+              <th>Unit Price</th>
+              <th>Qty</th>
+              <th>Net Amount</th>
+              <th>Tax Rate type and amount</th>
+              <th>Total Amount</th>
             </tr>
-          )}
-          {console.log(count)}
-          {count &&
-            Array(count)
-              .fill(" ")
-              .map((el, i) => {
-                console.log(el);
-                return (
-                  <React.Fragment key={i}>
-                    <tr>
-                      <td>1</td>
-                      <td>
-                        Varasiddhi Silks Men's Formal Shirt (SH-05-42, Navy
-                        Blue, 42)B07KGF3KW8 ( SH-05-42 )
-                      </td>
-                      <td>₹338.10</td>
-                      <td>1</td>
-                      <td>₹338.10</td>
-                      <td>2.5%</td>
-                      <td>CGST</td>
-                      <td>₹8.45</td>
-                      <td>₹365.00</td>
-                    </tr>
-                    <tr>
-                      <td></td>
-                      <td>Shipping Charges</td>
-                      <td>₹30.96</td>
-                      <td>1</td>
-                      <td>₹30.96</td>
-                      <td>2.5%</td>
-                      <td>CGST</td>
-                      <td>₹0.77</td>
-                      <td>₹32.50</td>
-                    </tr>
-                    <tr></tr>
-                  </React.Fragment>
-                );
-              })}
+          </thead>
+          <tbody>
+            {!count && (
+              <tr>
+                <td colSpan="9">
+                  <div className="flex justify-around my-20 ">
+                    <div>Please enter how many details you have</div>
+                    <input
+                      type="number"
+                      placeholder="how many orders you have"
+                      className="p-2 focus:outline-none focus:border-1 focus:border focus:border-gray-300"
+                      onChange={(e) => {
+                        console.log(e.target.value);
+                        setCount(Number(e.target.value));
+                        let listObj = [];
+                        Array(Number(e.target.value))
+                          .fill("")
+                          .map((el, i) => {
+                            listObj.push({
+                              id: i + 1,
+                              quantity: 0,
 
-          {/* <tr>
-            <td colSpan="8" className="">
-              Total:
-            </td>
-            <td>₹1,195.00</td>
-          </tr> */}
-        </tbody>
-      </table>
+                              Description: "",
+                              unitPrice: 0,
+                              fcgst: 0,
+                              fsgst: 0,
 
+                              unitTax: 0,
+
+                              scgst: 0,
+                              ssgst: 0,
+                            });
+                          });
+                        setTableData([...listObj]);
+                      }}
+                    />
+                  </div>
+                </td>
+              </tr>
+            )}
+            {console.log(count)}
+            {count &&
+              Array(count)
+                .fill(" ")
+                .map((el, i) => {
+                  return (
+                    <React.Fragment key={i}>
+                      <tr>
+                        <td>{i + 1}</td>
+                        <td>
+                          <textarea
+                            cols={40}
+                            rows={3}
+                            type="text"
+                            name="Description"
+                            onChange={(e) => {
+                              handlechange(e, i + 1);
+                            }}
+                            className="p-2  border-1 border-black border rounded mx-3"
+                            placeholder={`Varasiddhi Silks Men's Formal Shirt (SH-05-42, Navy  Blue, 42) \n B07KGF3KW8 ( SH-05-42 )`}
+                          />
+                        </td>
+                        <td>
+                          <input
+                            type="number"
+                            className="p-2  border-1 border-black border rounded mx-3"
+                            placeholder="Rs. 238 "
+                            name="unitPrice"
+                            onChange={(e) => {
+                              handlechange(e, i + 1);
+                            }}
+                          />
+                        </td>
+                        <td>
+                          <input
+                            type="number"
+                            className="p-2  border-1 border-black border rounded mx-3"
+                            placeholder="1"
+                            name="quantity"
+                            onChange={(e) => {
+                              handlechange(e, i + 1);
+                            }}
+                          />
+                        </td>
+                        <td>₹{tableData[i].netAmount || 0}</td>
+                        <td>
+                          <tr>
+                            <td>
+                              <input
+                                type="number"
+                                className="p-2  border-1 border-black border rounded mx-3"
+                                placeholder="2.5% "
+                                name="fcgst"
+                                onChange={(e) => {
+                                  handlechange(e, i + 1);
+                                }}
+                              />
+                            </td>
+                            <td>&nbsp;&nbsp; CGST &nbsp;&nbsp;</td>
+                            <td>
+                              &nbsp;&nbsp; ₹{tableData[i].ftaxcgstAmout}{" "}
+                              &nbsp;&nbsp;
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <input
+                                type="number"
+                                className="p-2  border-1 border-black border rounded mx-3"
+                                placeholder="2.5% "
+                                name="fsgst"
+                                onChange={(e) => {
+                                  handlechange(e, i + 1);
+                                }}
+                              />
+                            </td>
+                            <td>&nbsp;&nbsp; SGST &nbsp;&nbsp;</td>
+                            <td>
+                              &nbsp;&nbsp; ₹{tableData[i].ftaxsgstAmout}{" "}
+                              &nbsp;&nbsp;
+                            </td>
+                          </tr>
+                        </td>
+                        <td>
+                          &nbsp;&nbsp; ₹{tableData[i].ftotalAmount} &nbsp;&nbsp;
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td>{i + 1}</td>
+                        <td>Shipping Charges</td>
+                        <td>
+                          <input
+                            type="number"
+                            className="p-2  border-1 border-black border rounded mx-3"
+                            placeholder="Rs. 50 "
+                            name="unitTax"
+                            onChange={(e) => {
+                              handlechange(e, i + 1);
+                            }}
+                          />
+                        </td>
+                        <td></td>
+                        <td>₹{tableData[i].netTax || 0}</td>
+                        <td>
+                          <tr>
+                            <td>
+                              <input
+                                type="number"
+                                className="p-2  border-1 border-black border rounded mx-3"
+                                placeholder="2.5% "
+                                name="scgst"
+                                onChange={(e) => {
+                                  handlechange(e, i + 1);
+                                }}
+                              />
+                            </td>
+                            <td>&nbsp;&nbsp; CGST &nbsp;&nbsp;</td>
+                            <td>
+                              &nbsp;&nbsp; ₹{tableData[i].staxcgstAmout}{" "}
+                              &nbsp;&nbsp;
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <input
+                                type="number"
+                                className="p-2  border-1 border-black border rounded mx-3"
+                                placeholder="2.5% "
+                                name="ssgst"
+                                onChange={(e) => {
+                                  handlechange(e, i + 1);
+                                }}
+                              />
+                            </td>
+                            <td>&nbsp;&nbsp; SGST &nbsp;&nbsp;</td>
+                            <td>
+                              &nbsp;&nbsp; ₹{tableData[i].staxsgstAmout}{" "}
+                              &nbsp;&nbsp;
+                            </td>
+                          </tr>
+                        </td>
+                        <td>
+                          &nbsp;&nbsp; ₹{tableData[i].stotalAmount} &nbsp;&nbsp;
+                        </td>
+                      </tr>
+                    </React.Fragment>
+                  );
+                })}
+
+            <tr>
+              <td colSpan="8" className="">
+                Total:
+              </td>
+              <td>{total || 0}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <div className="amount-words">
         <strong>Amount in Words:</strong> One Thousand One Hundred And
         Ninety-five only
       </div>
 
       <p>Whether tax is payable under reverse charge - No</p>
-      <div className="mt-4 flex justify-between items-center">
+      <div className="mt-4 flex justify-between items-end">
         <div>
           <p>For Varasiddhi Silk Exports:</p>
+
+          <div className="flex justify-between items-center">
+            <div>
+              <p>click to select image for logo &darr;</p>
+              <label htmlFor="logo" className="">
+                <img
+                  src="https://via.placeholder.com/150x50"
+                  alt="Amazon Logo"
+                  className="h-12 hover:scale-105 hover:bg-black hover:cursor-pointer"
+                />
+              </label>
+              <input type="file" id="logo" hidden />
+            </div>
+          </div>
           <div className="mt-8">
             <p>Authorized Signatory</p>
           </div>
